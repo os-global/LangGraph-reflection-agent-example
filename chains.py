111a20b504c20f5e73060a7a97355e90db4b57d7
os-global <os-global@users.noreply.github.com>
2024-10-18 01:03:10 +0300

    switch to openai

--- chains.py
+++ chains.py
@@ -1,5 +1,10 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_mistralai import ChatMistralAI
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from openai import project
+
+load_dotenv()
 
 reflection_prompt = ChatPromptTemplate.from_messages(
     [
@@ -17,14 +22,14 @@ generation_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a twitter techie influencer assistent tasked with writing excellent twitter posts."
+            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts."
             " Generate the best twitter post possible for the user's request."
-            " If the user provides crituque, respond with a revised version of your previous attempts.",
+            " If the user provides critique, respond with a revised version of your previous attempts.",
         ),
         MessagesPlaceholder(variable_name="messages"),
     ]
 )
 
-llm = ChatMistralAI()
+llm = ChatOpenAI(model="gpt-3.5-turbo")
 generate_chain = generation_prompt | llm
 reflection_chain = reflection_prompt | llm
