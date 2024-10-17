d6cf7dc25541e65b4da7ff3ce894479ff8201252
os-global <os-global@users.noreply.github.com>
2024-10-18 01:03:35 +0300

    initial main with experiments

---
+++ main.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+from typing import List, Sequence
+
+load_dotenv()
+
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, MessageGraph
+
+from chains import reflection_chain, generate_chain
+
+REFLECT = "reflect"
+GENERATE = "generate"
+
+def generation_node(state: Sequence[BaseMessage]):
+    return generate_chain.invoke({"messages": state})
+
+
+def reflection_node(state: Sequence[BaseMessage]) -> List[BaseMessage]:
+    res = reflection_chain.invoke({"messages": state})
+    return [HumanMessage(content=res.content)]
+
+builder = MessageGraph()
+builder.add_node(GENERATE, generation_node)
+builder.add_node(REFLECT, reflection_node)
+builder.set_entry_point(GENERATE)
+
+def should_continue(state: List[BaseMessage]) -> bool:
+    if len(state) > 6:
+        return END
+    return REFLECT
+
+builder.add_conditional_edges(GENERATE, should_continue)
+builder.add_edge(REFLECT, GENERATE)
+
+graph = builder.compile()
+print(graph.get_graph().draw_mermaid())
+
+if __name__ == "__main__":
+    print("Hello LangGraph")
+    inputs = HumanMessage(content="""Make this tweet better:
+        @LangChainAI
+        - newly Tool Calling feature is seriously underrated.
+         
+         After a long wait, it's  here - making the implementation of agents across different models with function calling - super easy.
+         
+         Made a video covering their newest blog post""")
+
+    response = graph.invoke(inputs)
