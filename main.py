bf9a92a992546b3859ffacd86fa0516ad40a1630
os-global <os-global@users.noreply.github.com>
2024-10-19 00:20:45 +0300

    updated main

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from chains import reflection_chain, generate_chain
 REFLECT = "reflect"
 GENERATE = "generate"
 
+
 def generation_node(state: Sequence[BaseMessage]):
     return generate_chain.invoke({"messages": state})
 
@@ -19,15 +20,18 @@ def reflection_node(state: Sequence[BaseMessage]) -> List[BaseMessage]:
     res = reflection_chain.invoke({"messages": state})
     return [HumanMessage(content=res.content)]
 
+
 builder = MessageGraph()
 builder.add_node(GENERATE, generation_node)
 builder.add_node(REFLECT, reflection_node)
 builder.set_entry_point(GENERATE)
 
+
 def should_continue(state: List[BaseMessage]) -> bool:
     if len(state) > 6:
         return END
     return REFLECT
+
 
 builder.add_conditional_edges(GENERATE, should_continue)
 builder.add_edge(REFLECT, GENERATE)
@@ -36,7 +40,6 @@ graph = builder.compile()
 print(graph.get_graph().draw_mermaid())
 
 if __name__ == "__main__":
-    print("Hello LangGraph")
     inputs = HumanMessage(content="""Make this tweet better:
         @LangChainAI
         - newly Tool Calling feature is seriously underrated.
